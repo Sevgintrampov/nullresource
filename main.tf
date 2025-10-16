a0287d84966e4ada265d9f7b45bd114e1c2f6073
Sevgintrampov <168405248+Sevgintrampov@users.noreply.github.com>
2025-10-16 14:04:11 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,4 +2,5 @@ resource "null_resource" "cluster" {}
 resource "null_resource" "cluster2" {}
 resource "null_resource" "cluster3" {}
 resource "null_resource" "cluster38" {}
+resource "null_resource" "cluster388" {}
 #
